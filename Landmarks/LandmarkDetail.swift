083c77387aff6930b938c9aa41dea7e85992cbf9
Joshua Balloch <joshuaballoch@gmail.com>
2020-05-20 23:16:18 +0100

    Complete Building Lists and Navigation:
    * https://developer.apple.com/tutorials/swiftui/building-lists-and-navigation

--- Landmarks/LandmarkDetail.swift
+++ Landmarks/LandmarkDetail.swift
@@ -7,37 +7,43 @@
 //
 
 import SwiftUI
+import SwiftUI
 
-struct ContentView: View {
+struct LandmarkDetail: View {
+    var landmark: Landmark
+
     var body: some View {
         VStack {
-            MapView()
+            MapView(coordinate: landmark.locationCoordinate)
                 .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
-            CircleImage()
+
+            CircleImage(image: landmark.image)
                 .offset(y: -130)
                 .padding(.bottom, -130)
+
             VStack(alignment: .leading) {
-                Text("Turtle Rock")
+                Text(landmark.name)
                     .font(.title)
-                HStack {
-                    Text("Joshua Tree National Park")
+
+                HStack(alignment: .top) {
+                    Text(landmark.park)
                         .font(.subheadline)
                     Spacer()
-                    Text("California")
+                    Text(landmark.state)
                         .font(.subheadline)
                 }
-
             }
             .padding()
+
             Spacer()
         }
-            
+        .navigationBarTitle(Text(landmark.name), displayMode: .inline)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LandmarkDetail(landmark: landmarkData[0])
     }
 }
