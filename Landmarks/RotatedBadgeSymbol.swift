46f6896ccc0c7ece2884db846849c844c8322118
Joshua Balloch <joshuaballoch@gmail.com>
2020-05-21 08:00:32 +0100

    Complete drawing shapes:
    * Add a mountain-like BadgeSymbol
    * Move badge to back background
    * Add a RotatedBadgeSymbol
    * Draw many of them in a circle to show a sun shape
    
    Completes: https://developer.apple.com/tutorials/swiftui/drawing-paths-and-shapes

---
+++ Landmarks/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Joshua Balloch on 5/21/20.
+//  Copyright © 2020 Thimble. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
