c65073e20b026dcda309c2daa223aae4f1f06c67
Joshua Balloch <joshuaballoch@gmail.com>
2020-05-24 16:58:37 +0100

    Complete CategoryHome:
    https://developer.apple.com/tutorials/swiftui/composing-complex-interfaces

--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -32,9 +32,12 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
+            NavigationView {
+                LandmarkList()
+                    .environmentObject(UserData())
+                    .previewDevice(PreviewDevice(rawValue: deviceName))
+                    .previewDisplayName(deviceName)
+            }
         }
     }
 }
